00a16dd15ed076b7aa359da093f7c4b0aa7fbc9c
Anna Alyoshina <denielaz2020@gmail.com>
2023-04-25 12:01:40 +0300

    setting to redux store

--- src/components/Form/hooks.ts
+++ src/components/Form/hooks.ts
@@ -1,7 +1,11 @@
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
 import { IGithubApi } from "../../typings/IGithubApi";
+import { setCurrentRepo } from "../../store/slices/repo";
 
 export const useGetRepo = (api: IGithubApi, owner: string, repo: string) => {
+  const dispatch = useDispatch();
+
   return () => {
     api.getRepo(owner, repo)
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -9,6 +13,7 @@ export const useGetRepo = (api: IGithubApi, owner: string, repo: string) => {
         if (data.message) {
           toast.error('404 Not Found');
         }
+        dispatch(setCurrentRepo(data));
         console.log(data);
       })
       .catch(console.log);
